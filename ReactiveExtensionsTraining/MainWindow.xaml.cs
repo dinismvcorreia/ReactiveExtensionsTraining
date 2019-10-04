7f74a84fed4aefad4f5758e2554d8946402739fd
Dinis Correia <dinis.correia@faro.com>
2019-10-04 18:56:35 +0100

    Add Where and Select operators and Schedulers

--- ReactiveExtensionsTraining/MainWindow.xaml.cs
+++ ReactiveExtensionsTraining/MainWindow.xaml.cs
@@ -3,7 +3,9 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reactive.Concurrency;
     using System.Reactive.Linq;
+    using System.Threading;
     using System.Windows;
 
     /// <summary>
@@ -11,7 +13,7 @@
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly IEnumerable<int> _numbers;
+        private readonly IEnumerable<(int Number, int ThreadId)> _numbers;
 
         public MainWindow()
         {
@@ -20,9 +22,19 @@
             _numbers = GetNumbersFromSource();
         }
 
-        private static IEnumerable<int> GetNumbersFromSource()
+        private static IEnumerable<(int, int)> GetNumbersFromSource()
         {
-            return Enumerable.Range(1, 10);
+            return Enumerable
+                    .Range(1, 10)
+                    .Select(n =>
+                    {
+                        Thread.Sleep(250);
+
+                        //if (n == 5)
+                        //    throw new InvalidOperationException("Oops!");
+
+                        return (n, Thread.CurrentThread.ManagedThreadId);
+                    });
         }
 
         private void PullButton_Click(object sender, RoutedEventArgs e)
@@ -41,13 +53,21 @@
         private void ObserveButton_Click(object sender, RoutedEventArgs e)
         {
             ItemsListBox.Items.Clear();
-            ItemsListBox.Items.Add("*** Observing Data ***");
+            ItemsListBox.Items.Add($"*** Observing Data on Thread {Thread.CurrentThread.ManagedThreadId} ***");
 
             var numbersObs = _numbers.ToObservable();
 
-            numbersObs.Subscribe(
-                n => ItemsListBox.Items.Add(n),
-                () => ItemsListBox.Items.Add("Completed!"));
+            numbersObs
+                //.Where(tuple => tuple.Number % 2 == 0)
+                .Select(tuple => $"{tuple.Number} on Thread {tuple.ThreadId}")
+                //.SubscribeOn(NewThreadScheduler.Default)
+                //.ObserveOnDispatcher()
+                .Subscribe(
+                    n => ItemsListBox.Items.Add(n),
+                    //ex => ItemsListBox.Items.Add(ex.Message),
+                    () => ItemsListBox.Items.Add("Completed!"));
+
+            //ItemsListBox.Items.Add("Completed!");
         }
     }
 }
